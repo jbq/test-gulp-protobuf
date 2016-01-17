fea13a02fb15e33c8502008d500929300473bdb7
Jean-Baptiste Quenot <jbq@caraldi.com>
2016-01-17 10:01:58 +0100

    Use gulp-pbjs

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
-var gulpprotobuf = require('gulp-protobufjs');
+//var gulpprotobuf = require('gulp-protobufjs');
+var gulppbjs = require('gulp-pbjs');
 
 gulp.task('default', function () {
   return gulp.src('files/organization.proto')
-    .pipe(gulpprotobuf())
+    //.pipe(gulpprotobuf())
+    .pipe(gulppbjs({path:"files",target:"js",source:"proto"}))
     .pipe(gulp.dest('out/'));
 });
